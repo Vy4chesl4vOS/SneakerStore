743f9d497b0a466376021557400bd1d5fff3678b
Vy4chesl4vOS <vy4chesl4v.u@icloud.com>
2022-05-01 14:48:25 +0300

    Initial Commit

---
+++ SneakerStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SneakerStore
+//
+//  Created by Вячеслав Утешев on 01.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SneakerStore/SneakerStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SneakerStoreApp.swift
+//  SneakerStore
+//
+//  Created by Вячеслав Утешев on 01.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SneakerStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
